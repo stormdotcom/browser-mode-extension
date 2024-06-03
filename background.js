40a30c956840ce291149a62c4812229fb8b88736
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-06-03 13:04:41 +0530

    basic MVP added

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ mode: "work" });
+  chrome.storage.sync.set({
+    mode: "work", // default mode
+    workUrls: ["https://gmail.google.com", "https://jira.com"],
+    personalUrls: ["https://youtube.com", "https://scaler.com"],
+  });
 });
 
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.toggleMode === "work") {
-    chrome.storage.sync.set({ mode: "work" });
-  } else if (message.toggleMode === "personal") {
-    chrome.storage.sync.set({ mode: "personal" });
-  }
+chrome.windows.onCreated.addListener(() => {
+  chrome.storage.sync.get(["mode", "workUrls", "personalUrls"], (result) => {
+    const urls = result.mode === "work" ? result.workUrls : result.personalUrls;
+    for (const url of urls) {
+      chrome.tabs.create({ url: url });
+    }
+  });
 });
