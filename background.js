4565ee943886c1483003c72b0270647f5c585cce
Ajmal Nasumudeen <ajmaln73@gmail.com>
2024-06-07 10:40:29 +0530

    modified url opening method, removed tabs permission

--- background.js
+++ background.js
@@ -7,13 +7,27 @@ chrome.runtime.onInstalled.addListener(() => {
     personalUrls: ["https://youtube.com", "https://chatgpt.com", "https://www.scaler.com/academy/mentee-dashboard/todos"],
   });
 });
+
+
 chrome.runtime.onStartup.addListener(() => {
   chrome.storage.sync.get(["mode", "workUrls", "personalUrls"], (result) => {
     const urls = result.mode === "Work" ? result.workUrls : result.personalUrls;
-    for (const url of urls) {
-      chrome.tabs.create({ url: url });
-    }
+    openUrls(urls);
   });
 });
 
-
+function openUrls(urls) {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    if (tabs.length > 0) {
+      chrome.tabs.update(tabs[0].id, { url: urls[0] }, () => {
+        for (let i = 1; i < urls.length; i++) {
+          chrome.tabs.create({ url: urls[i] });
+        }
+      });
+    } else {
+      for (const url of urls) {
+        chrome.tabs.create({ url: url });
+      }
+    }
+  });
+}
